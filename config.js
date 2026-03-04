313853deb02a0ecc76b90e86123dbcb59ca53f85
trashcore-dev <trashcoreclient@gmail.com>
2026-03-04 18:40:00 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    AUTO_VIEW_STATUS: 'true',
+    AUTO_LIKE_STATUS: 'true',
+    AUTO_RECORDING: 'true',
+    AUTO_LIKE_EMOJI: ['🧩', '🍉', '💜', '🌸', '🪴', '💊', '💫', '🍂', '🌟', '🎋', '😶‍🌫️', '🫀', '🧿', '👀', '🤖', '🚩', '🥰', '🗿', '💜', '💙', '🌝', '🖤', '💚'],
+    PREFIX: '.',
+    MAX_RETRIES: 3,
+    GROUP_INVITE_LINK: 'https://chat.whatsapp.com/JqGh3LtWerw35NILvYQlFb',
+    ADMIN_LIST_PATH: './admin.json',
+    IMAGE_PATH: 'https://files.catbox.moe/dc5opm.jpg',
+    NEWSLETTER_JID: '120363322464215140@newsletter,
+    NEWSLETTER_MESSAGE_ID: '428',
+    OTP_EXPIRY: 300000,
+    NEWS_JSON_URL: '',
+    BOT_NAME: '⟆⟆𝗧𝗥𝗔𝗦𝗛𝗖𝗢𝗥𝗘 𝗠𝗗  ⟅⟅',
+    OWNER_NAME: '𝗧𝗥𝗔𝗦𝗛',
+    OWNER_NUMBER: '254703726139',
+    BOT_VERSION: '2.0.0',
+    BOT_FOOTER: '> © Powered by Trashcore',
+    CHANNEL_LINK: 'https://whatsapp.com/channel/0029VafrbsKG8l5EFBDDCy41'
+};
